0f39ec25abaced9fee952e513060e5ca8897901b
Anthony Rhony Ramon <rhony.pro@gmail.com>
2023-05-22 10:47:41 +0200

    Create push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,11 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include "tools/tools.h"
+#include "libft/libft.h"
+
+void    fill(int *all, char **argv);
+void    cut_args(int *all, t_list **pile_a, int max);
+int     get_int_amount(char **argv);
+
+#endif
